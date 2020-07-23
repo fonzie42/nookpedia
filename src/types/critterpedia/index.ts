672812c619bf2beeb95852c8f9141a6045ff0ba5
Afonso <ferrer.afonso@gmail.com>
2020-07-23 20:08:18 -0300

    [bugs] Added available Localization strings for Critter

--- src/types/critterpedia/index.ts
+++ src/types/critterpedia/index.ts
@@ -1,2 +1,19 @@
+export interface LocalizationString {
+  'de-EU': string
+  'en-EU': string
+  'en-US': string
+  'es-EU': string
+  'es-US': string
+  'fr-EU': string
+  'fr-US': string
+  'it-EU': string
+  'ja-JP': string
+  'ko-KR': string
+  'nl-EU': string
+  'ru-EU': string
+  'zh-CN': string
+  'zh-TW': string
+}
+
 export type Hemisphere = 'Southern' | 'Northern'
 
