80e58d6fa771c1d8ead85ce2840458c14ddda3d8
Afonso <ferrer.afonso@gmail.com>
2020-07-23 20:53:58 -0300

    [bugs] Added function to get bugs from local storage

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -1,3 +1,4 @@
+import { UserCritterpediaData } from 'types/critterpedia/personal'
 import { UserCritterPediaBugs } from 'types/critterpedia/bug'
 
 export const persistBugsInLocalStorage = (
@@ -14,3 +15,12 @@ export const persistBugsInLocalStorage = (
   window.localStorage.setItem(`critter-pedia-bugs`, stringfied)
 }
 
+export const getBugsFromLocalStorage = (): UserCritterpediaData[] => {
+  const rawData = window.localStorage.getItem(`critter-pedia-bugs`)
+
+  if (!rawData) {
+    return []
+  }
+
+  return JSON.parse(rawData) as UserCritterpediaData[]
+}
