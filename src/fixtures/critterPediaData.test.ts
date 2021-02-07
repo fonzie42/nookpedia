f0cfade0451b473553e363a4be64ba63d611be69
Afonso Ferrer <ferrer.afonso@gmail.com>
2021-02-06 21:31:29 -0300

    [filters] Added CritterPedia fixture generator

---
+++ src/fixtures/critterPediaData.test.ts
@@ -0,0 +1,54 @@
+import BUGS from 'data/critterpedia/bugs'
+import { createCritterPediaData } from './critterPediaData'
+
+describe('createCritterPediaData', () => {
+  it('returns the array of desired length with unaltared properties', () => {
+    const length = 4
+    const createdFixtures = createCritterPediaData({ length })
+    expect(createdFixtures.length).toEqual(length)
+
+    const dataWithCritter = BUGS.bugs.slice(0, length).map((item) => ({
+      ...item,
+      isDonatedToMuseum: false,
+      isRegisteredOnCritterPedia: false,
+    }))
+
+    expect(createdFixtures).toEqual(dataWithCritter)
+  })
+
+  it('returns an array with all items donatedToMuseum set to true', () => {
+    const length = 4
+    const createdFixtures = createCritterPediaData({
+      length,
+      isDonatedToMuseum: true,
+    })
+
+    createdFixtures.map((item) => expect(item.isDonatedToMuseum).toBeTruthy())
+  })
+
+  it('returns an array with all items isRegisteredOnCritterPedia to true', () => {
+    const length = 4
+    const createdFixtures = createCritterPediaData({
+      length,
+      isRegisteredOnCritterPedia: true,
+    })
+
+    createdFixtures.map((item) =>
+      expect(item.isRegisteredOnCritterPedia).toBeTruthy(),
+    )
+  })
+
+  it('returns an array with custom availability for nouthern and southern hemishphere', () => {
+    const length = 4
+    const monthsAvailability = [1]
+    const createdFixtures = createCritterPediaData({
+      length,
+      monthsAvailability,
+    })
+
+    createdFixtures.map(({ availability: { northern, southern } }) => {
+      expect(northern).toEqual(monthsAvailability)
+      expect(southern).toEqual(monthsAvailability)
+    })
+  })
+})
---
+++ src/fixtures/critterPediaData.ts
@@ -0,0 +1,29 @@
+import BUGS_CRITTER from 'data/critterpedia/bugs'
+import { UserCritterPediaBugs } from 'types/critterpedia/bug'
+
+type createCritterPediaDataProps = {
+  length: number
+  isDonatedToMuseum?: boolean
+  isRegisteredOnCritterPedia?: boolean
+  monthsAvailability?: number[]
+}
+
+export const createCritterPediaData = ({
+  length,
+  isDonatedToMuseum = false,
+  isRegisteredOnCritterPedia = false,
+  monthsAvailability,
+}: createCritterPediaDataProps) =>
+  BUGS_CRITTER.bugs.slice(0, length).map(
+    (item) =>
+      ({
+        ...item,
+        isRegisteredOnCritterPedia: isRegisteredOnCritterPedia,
+        isDonatedToMuseum: isDonatedToMuseum,
+        availability: {
+          ...item.availability,
+          southern: monthsAvailability ?? item.availability.southern,
+          northern: monthsAvailability ?? item.availability.northern,
+        },
+      } as UserCritterPediaBugs),
+  )
