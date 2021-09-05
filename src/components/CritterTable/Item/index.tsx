695d46b6f1df38d69a4393e3f0eadf93a41c04da
Afonso Ferrer <ferrer.afonso@gmail.com>
2021-09-05 17:00:22 -0300

    [critter-components] Updated CritterItem to have styling

--- src/components/CritterTable/Item/index.tsx
+++ src/components/CritterTable/Item/index.tsx
@@ -2,15 +2,20 @@ import { UserCritterPediaData } from 'types/critterpedia'
 
 import { ItemProps } from './types'
 
+import './item.scss'
+
 export function Item<T extends UserCritterPediaData>({
   critter,
 }: ItemProps<T>): JSX.Element {
   return (
-    <div>
-      <img
-        src={process.env.PUBLIC_URL + `/assets/icons${critter.filePath}.png`}
-        alt={critter.name['en-US']}
-      />
+    <div className="critter-card-item">
+      <button className="critter-card-item__button">
+        <img
+          className="critter-card-item__image"
+          src={process.env.PUBLIC_URL + `/assets/icons${critter.filePath}.png`}
+          alt={critter.name['en-US']}
+        />
+      </button>
     </div>
   )
 }
