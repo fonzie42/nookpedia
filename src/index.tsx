526729b45e0089e613f6ccc82df06597c4c793a0
Afonso Ferrer <ferrer.afonso@gmail.com>
2022-04-25 00:47:32 -0300

    [refactors] Updated Global styles to use styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 
+import { GlobalStyle } from 'app.styled'
 import ReactDOM from 'react-dom'
-import './index.scss'
 import App from 'scenes/App'
 import * as serviceWorker from 'serviceWorker'
 
 ReactDOM.render(
   <StrictMode>
+    <GlobalStyle />
     <App />
   </StrictMode>,
   document.getElementById('root'),
