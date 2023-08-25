679ba4065d2edd98dd14bb61254b863c558476f1
Afonso <ferrer.afonso@gmail.com>
2023-08-25 19:58:02 -0300

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,18 @@ import viteTsconfigPaths from 'vite-tsconfig-paths'
 import svgrPlugin from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-  server: {
-    open: true,
-  },
+export default defineConfig(({ command }) => {
+  const config = {
+    base: '/',
+    plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+    server: {
+      open: true,
+    },
+  }
+
+  if (command !== 'serve') {
+    config.base = '/nookpedia/'
+  }
+
+  return config
 })
